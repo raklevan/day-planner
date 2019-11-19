30f5b46d8cdaa7a74cb5c2d80bb20f1ca6f68b25
Larry <richardklevan@gmail.com>
2019-11-19 08:10:51 -0700

    uploading final draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,16 @@ var now = $("#time");
 
 setInterval(function () {
     now.text(moment().format("MMM Do YYYY - hh:mm:ss"));
+
 }, 1000);
 
 
+var currentTime = moment()
 
-$("#9-btn").click(function(event){
+
+
+
+$("#9-btn").click(function (event) {
     event.preventDefault();
     var input9 = $("#9").val()
     localStorage.setItem("@9:00", input9)
@@ -17,14 +22,18 @@ $("#9-btn").click(function(event){
 
     console.log(localStorage.getItem("@9:00"))
 
-//    document.querySelector(".text9").textContent=localStorage.getItem("@9:00");
+    console.log(currentTime)
+
+    //    document.querySelector(".text9").textContent=localStorage.getItem("@9:00");
 
 
 })
 
-function save9(){
-if(localStorage.getItem("@9:00")){
-    $("#9").val(localStorage.getItem("@9:00"))}}
+function save9() {
+    if (localStorage.getItem("@9:00")) {
+        $("#9").val(localStorage.getItem("@9:00"))
+    }
+}
 
 save9()
 
@@ -32,22 +41,24 @@ save9()
 
 
 
-$("#10-btn").click(function(event){
+$("#10-btn").click(function (event) {
     event.preventDefault();
     var input10 = $("#10").val()
     localStorage.setItem("@10:00", input10)
     console.log(input10);
 
 })
-function save10(){
-    if(localStorage.getItem("@10:00")){
-        $("#10").val(localStorage.getItem("@10:00"))}}
-    
-    save10()
+function save10() {
+    if (localStorage.getItem("@10:00")) {
+        $("#10").val(localStorage.getItem("@10:00"))
+    }
+}
+
+save10()
 
 
 
-$("#11-btn").click(function(event){
+$("#11-btn").click(function (event) {
     event.preventDefault();
     var input11 = $("#11").val()
     localStorage.setItem("@11:00", input11)
@@ -55,15 +66,17 @@ $("#11-btn").click(function(event){
 
 })
 
-function save11(){
-    if(localStorage.getItem("@11:00")){
-        $("#11").val(localStorage.getItem("@11:00"))}}
-    
-    save11()
+function save11() {
+    if (localStorage.getItem("@11:00")) {
+        $("#11").val(localStorage.getItem("@11:00"))
+    }
+}
+
+save11()
 
 
 
-$("#12-btn").click(function(event){
+$("#12-btn").click(function (event) {
     event.preventDefault();
     var input12 = $("#12").val()
     localStorage.setItem("@12:00", input12)
@@ -71,15 +84,17 @@ $("#12-btn").click(function(event){
 
 })
 
-function save12(){
-    if(localStorage.getItem("@12:00")){
-        $("#12").val(localStorage.getItem("@12:00"))}}
-    
-    save12()
+function save12() {
+    if (localStorage.getItem("@12:00")) {
+        $("#12").val(localStorage.getItem("@12:00"))
+    }
+}
+
+save12()
 
 
 
-$("#1-btn").click(function(event){
+$("#1-btn").click(function (event) {
     event.preventDefault();
     var input1 = $("#1").val()
     localStorage.setItem("@1:00", input1)
@@ -87,15 +102,17 @@ $("#1-btn").click(function(event){
 
 })
 
-function save1(){
-    if(localStorage.getItem("@1:00")){
-        $("#1").val(localStorage.getItem("@1:00"))}}
-    
-    save1()
+function save1() {
+    if (localStorage.getItem("@1:00")) {
+        $("#1").val(localStorage.getItem("@1:00"))
+    }
+}
+
+save1()
 
 
 
-$("#2-btn").click(function(event){
+$("#2-btn").click(function (event) {
     event.preventDefault();
     var input2 = $("#2").val()
     localStorage.setItem("@2:00", input2)
@@ -103,30 +120,34 @@ $("#2-btn").click(function(event){
 
 })
 
-function save2(){
-    if(localStorage.getItem("@2:00")){
-        $("#2").val(localStorage.getItem("@2:00"))}}
-    
-    save2()
+function save2() {
+    if (localStorage.getItem("@2:00")) {
+        $("#2").val(localStorage.getItem("@2:00"))
+    }
+}
+
+save2()
 
 
 
-$("#3-btn").click(function(event){
+$("#3-btn").click(function (event) {
     event.preventDefault();
     var input3 = $("#3").val()
     localStorage.setItem("@3:00", input3)
     console.log(input3);
 
 })
-function save3(){
-    if(localStorage.getItem("@3:00")){
-        $("#3").val(localStorage.getItem("@3:00"))}}
-    
-    save3()
+function save3() {
+    if (localStorage.getItem("@3:00")) {
+        $("#3").val(localStorage.getItem("@3:00"))
+    }
+}
+
+save3()
 
 
 
-$("#4-btn").click(function(event){
+$("#4-btn").click(function (event) {
     event.preventDefault();
     var input4 = $("#4").val()
     localStorage.setItem("@4:00", input4)
@@ -134,13 +155,15 @@ $("#4-btn").click(function(event){
 
 })
 
-function save4(){
-    if(localStorage.getItem("@4:00")){
-        $("#4").val(localStorage.getItem("@4:00"))}}
-    
-    save4()
+function save4() {
+    if (localStorage.getItem("@4:00")) {
+        $("#4").val(localStorage.getItem("@4:00"))
+    }
+}
 
-$("#5-btn").click(function(event){
+save4()
+
+$("#5-btn").click(function (event) {
     event.preventDefault();
     var input5 = $("#5").val()
     localStorage.setItem("@5:00", input5)
@@ -148,11 +171,34 @@ $("#5-btn").click(function(event){
 
 })
 
-function save5(){
-    if(localStorage.getItem("@5:00")){
-        $("#5").val(localStorage.getItem("@5:00"))}}
-    
-    save5()
+function save5() {
+    if (localStorage.getItem("@5:00")) {
+        $("#5").val(localStorage.getItem("@5:00"))
+    }
+}
+
+save5()
+
+
+function styleTime() {
+    for (let index = 9; index < 18; index++) {
+
+        // console.log($("#900").attr("id"))
+        if (100 * moment().format("H") < $("#" + 100 * index).attr("id")) {
+            $("#" + 100 * index).addClass("future");
+        }
+
+
+        else if (100 * moment().format("H") > $("#" + 100 * index).attr("id")) {
+            $("#" + 100 * index).addClass("past");
+
+
+        }
+
+    }
+}
+
+styleTime()
 
 
 
